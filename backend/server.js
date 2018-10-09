1ff2cab6a9314b588a984b19c51a8cc7e89544e6
Tore Sæstad <tore.saestad@appeartv.com>
2018-10-09 09:41:48 +0200

    Update CV

--- backend/server.js
+++ backend/server.js
@@ -9,17 +9,17 @@ app.get('/', (request, response) => {
 });
 
 app.get('/api/me', (request, response) => {
-	response.send('{"name": "Arthur Dent", "location": "Earth"}');
+	response.send('{"name": "Tore Havsø Sæstad", "location": "Oslo, Norway"}');
 	logger.info('Responded to GET request on /api/me')
 });
 
 app.get('/api/work', (request, response) => {
-	response.send('[{"yearFrom": "2010", "yearTo": "2018", "place": "BBC Radio", "comment": "Was doing stuff"}, {"yearFrom": "2009", "yearTo": "2010", "place": "The coffee shop", "comment": "Making coffee"}]');
+	response.send('[{"yearFrom": "2014", "yearTo": "2018", "place": "Appear TV", "comment": "R&D Engineer"}]');
 	logger.info('Responded to GET request on /api/work')
 });
 
 app.get('/api/education', (request, response) => {
-	response.send('[{"yearFrom": "2005", "yearTo": "2007", "place": "University 1", "comment": "Studying"}, {"yearFrom": "2001", "yearTo": "2004", "place": "Barista academy", "comment": "Made coffee"}]');
+	response.send('[{"yearFrom": "2009", "yearTo": "2014", "place": "NTNU", "comment": "Electronics"}]');
 	logger.info('Responded to GET request on /api/edu')
 });
 
